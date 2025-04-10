4eed6daa4b7757036fbb8147441ba38aa3ccb66d
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-04-10 16:50:12 +0700

    Admin Backend Slider Option - Part 1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\SliderController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,6 +25,12 @@ Route::middleware('auth')->group(function () {
     Route::post('/user/password/update', [ProfileController::class, 'PasswordUpdate'])->name('user.password.update');
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::controller(SliderController::class)->group(function(){
+        Route::get('/all/slider', 'AllSlider')->name('all.slider');
+    });
 });
 
 require __DIR__.'/auth.php';
