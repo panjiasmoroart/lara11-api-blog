0eb554bcc2599c8b6a5e9fe23261f917d8a80ec5
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-04-11 14:56:31 +0700

    Admin Backend Services Option - Part 3

--- app/Http/Controllers/Backend/ServiceController.php
+++ app/Http/Controllers/Backend/ServiceController.php
@@ -20,4 +20,34 @@ class ServiceController extends Controller
     {
         return view('backend.service.add_service');
     }
+
+    public function StoreService(Request $request){
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(688,436)->save(public_path('upload/service/'.$name_gen));
+            $save_url = 'upload/service/'.$name_gen;
+
+            Service::create([
+                'service_name' => $request->service_name,
+                'slug' => strtolower(str_replace(' ', '-', $request->service_name)),
+                'service_short' => $request->service_short,
+                'service_desc' => $request->service_desc,
+                'icon' => $request->icon,
+                'image' => $save_url,
+            ]);
+        }
+
+        $notification = array(
+            'message' => 'Service Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.service')->with($notification);
+
+    }
+
 }
